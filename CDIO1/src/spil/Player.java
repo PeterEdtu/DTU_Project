ba56e0820662834ca560d05110d8db49445b6beb
Daniel Larsen <iyyel@users.noreply.github.com>
2016-09-30 12:47:10 +0200

    Update Player.java

--- CDIO1/src/spil/Player.java
+++ CDIO1/src/spil/Player.java
@@ -1,57 +1,64 @@
-/*
- * Player Class
- * @author PeterElHabr
- * 40_CDIO1
+/**
+ * @author Gruppe 40
+ * CDIO del 1
+ * 
+ * Daniel Anusic
+ * Daniel Larsen
+ * Peter Issam El-Habr
+ * Loui Southwick
+ * Nemanja Stefan Mitrovic
  */
+
 package spil;
 
 public class Player {
-	
-	private static Dice D1 = new Dice();
-	private static Dice D2 = new Dice();
-	
+
+	private static Dice d1 = new Dice();
+	private static Dice d2 = new Dice();
+
 	private int score;
 	private String name;
-	private final int MAX_SCORE=40;
-	
+	private final int MAX_SCORE = 40;
+
 	/**
 	 * Create a player who can roll
 	 * @author PeterElHabr
 	 * @param name : Define the player's name
 	 */
-	public Player(String name){
-		this.score=0;
-		this.name=name;
+	public Player(String name) {
+		this.score = 0;
+		this.name = name;
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		String showscore;
-		if (this.Win()==false)
-		showscore=this.name+"'s score is: "+this.score;
-		else showscore=this.name + " wins with "+this.score;
+		if (this.win() == false)
+			showscore = this.name + "'s score is: " + this.score;
+		else
+			showscore = this.name + " wins with " + this.score;
 		return showscore;
 	}
-	
+
 	/**
 	 * Add (amount>0) points to score
 	 * Remove (amount<0)
 	 * @param amount
 	 */
-	private void AddToScore(int amount){
-		this.score=this.score+amount;
+	private void addToScore(int amount) {
+		this.score = this.score + amount;
 	}
-	
-	public boolean Win(){
-		if (this.score>=MAX_SCORE){
+
+	public boolean win() {
+		if (this.score >= MAX_SCORE) {
 			return true;
-		}else return false;
+		} else {
+			return false;
+		}
 	}
-	
-	public void Play(){
-		D1.roll();
-		D2.roll();
-		this.AddToScore(D1.getFaceValue()+D2.getFaceValue());
+
+	public void play() {
+		d1.roll();
+		d2.roll();
+		this.addToScore(d1.getFaceValue() + d2.getFaceValue());
 	}
 }
-
-
