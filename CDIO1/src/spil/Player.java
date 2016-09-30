04337670c892ea2c40b2de142a7440022ac4bbd1
Daniel Larsen <iyyel@users.noreply.github.com>
2016-09-30 15:39:16 +0200

    Update Player.java

--- CDIO1/src/spil/Player.java
+++ CDIO1/src/spil/Player.java
@@ -1,7 +1,7 @@
 /**
  * @author Gruppe 40
  * CDIO del 1
- * @version 1.0
+ * @version 1.1
  * 
  * Daniel Anusic
  * Daniel Larsen
@@ -12,37 +12,33 @@
 
 package spil;
 
-import java.awt.List;
-import java.util.ArrayList;
-
 public class Player {
 
 	private static Dice d1 = new Dice();
 	private static Dice d2 = new Dice();
 
-	private ArrayList<String> result = new ArrayList<String> ();
-	public int id;
+	private final int MAX_SCORE = 40;
+	private int id;
 	private int score;
 	private String name;
-	private final int MAX_SCORE = 40;
 
 	/**
 	 * Create a player who can roll
 	 * @author PeterElHabr
 	 * @param name : Define the player's name
 	 */
-	public Player(String name) {
+	public Player(String name, int id) {
+		this.id = id;
 		this.score = 0;
 		this.name = name;
 	}
 
 	public String toString() {
-		String showscore;
-		if (this.win() == false)
-			showscore = "["+id+"]"+this.name + "'s score is: " + this.score+" {+"+result+"}";
+		String showScore;
+		if (this.win() == false) showScore = "[" + id + "] " + this.name + "'s score is now: " + this.score + "\n";
 		else
-			showscore = "["+id+"]"+this.name + " wins with " + this.score+"\n-----------------------------------------";
-		return showscore;
+			showScore = "[" + id + "] " + this.name + " wins with " + this.score + "!\n-----------------------------------------";
+		return showScore;
 	}
 
 	/**
@@ -51,7 +47,7 @@ public class Player {
 	 * @param amount
 	 */
 	private void addToScore(int amount) {
-		this.score = this.score + amount;
+		this.score += amount;
 	}
 
 	public boolean win() {
@@ -62,16 +58,28 @@ public class Player {
 		}
 	}
 
+	public int playOnce() {
+		return d1.roll();
+	}
+
 	public void play() {
-		result.clear();
 		d1.initiate();
 		d2.initiate();
-		while (d1.getFaceValue()==d2.getFaceValue()){
+		while (d1.getFaceValue() == d2.getFaceValue()) {
 			d1.roll();
 			d2.roll();
-			result.add(""+d1);
-			result.add(""+d2);
+			System.out.println("[" + id + "] " + name + " rolled " + d1 + " and " + d2 + "!");
 			this.addToScore(d1.getFaceValue() + d2.getFaceValue());
 		}
+		System.out.println();
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 }
