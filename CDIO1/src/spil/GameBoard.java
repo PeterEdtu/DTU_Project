da002f58ab66e684ac0b322f1fcaf1ac81cfcba5
Daniel Larsen <iyyel@users.noreply.github.com>
2016-09-30 12:47:56 +0200

    Create GameBoard.java

---
+++ CDIO1/src/spil/GameBoard.java
@@ -0,0 +1,18 @@
+/**
+ * @author Gruppe 40
+ * CDIO del 1
+ * 
+ * Daniel Anusic
+ * Daniel Larsen
+ * Peter Issam El-Habr
+ * Loui Southwick
+ * Nemanja Stefan Mitrovic
+ */
+
+package spil;
+
+public class GameBoard {
+
+	
+
+}
