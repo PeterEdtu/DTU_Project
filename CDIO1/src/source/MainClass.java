2b7e46d49f03bf5647c51ef335ab038a5845e849
Peter <peterxp@hotmail.fr>
2016-09-30 08:25:34 +0200

    Update MainClass.java

--- CDIO1/src/source/MainClass.java
+++ CDIO1/src/source/MainClass.java
@@ -1,9 +1,17 @@
+/*
+ * @author Team 40
+ * CDIO1
+ * 
+ * 
+ */
+
+
 package source;
 
 public class MainClass {
-	
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
 	}
 
