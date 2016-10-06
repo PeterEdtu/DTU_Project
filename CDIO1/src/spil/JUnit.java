26904aa15e10636d71cf687e6490d1129a8ff089
Peter <peterxp@hotmail.fr>
2016-10-06 11:22:12 +0200

    Update JUnit.java

--- CDIO1/src/spil/JUnit.java
+++ CDIO1/src/spil/JUnit.java
@@ -41,111 +41,119 @@ public class JUnit {
 	public final void testPlayerAddToScore() {
 		int expected = 5;
 		int amount = 5;
-		
+
 		player1.addToScore(amount);
-		
+
 		int result = player1.getScore();
-		
+
 		assertEquals(expected, result);
-		
+
 		player1.addToScore(8);
-		
+
 		expected=13;
 		result=player1.getScore();
 		assertEquals(expected, result);
-		
+
 		player1.addToScore(-8);
-		
+
 		expected=5;
 		result=player1.getScore();
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
 	public final void testPlayerAddToScoreOverflow() {
 		//System.out.println(Integer.MAX_VALUE);
 		int expected = 2147483647;
 		int amount = Integer.MAX_VALUE+1;
-		
+
 		player1.addToScore(amount);
-		
+
 		int result = player1.getScore();
-		
+
 		//System.out.println(result);
-		
+
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
 	public final void testPlayerAddToScoreUnderflow() {
 		int expected = -2147483647;
 		int amount = Integer.MIN_VALUE-1;
-		
+
 		player1.addToScore(amount);
-		
+
 		int result = player1.getScore();
-		
+
 		//System.out.println(result);
-		
+
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
 	public final void testPlayerHasWon() {
 		//Begin
 		boolean expected = false;
 		boolean result;
-		
+
 		result = player1.hasWon();
-		
+
 		assertEquals(expected, result);
-		
+
 		//Test
 		player1.addToScore(39);
-		
+
 		result = player1.hasWon();
-		
+
 		assertEquals(expected, result);
-		
+
 		//Test2
 		expected=true;
 		player1.addToScore(1);
-		
+
 		result = player1.hasWon();
-		
+
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
 	public final void testDiceCupShakeOneDie(){
 		boolean expected = true;
 		boolean result;
-		int rollValue=diceCup.shakeOneDie(player1);
-		result=(0<rollValue && rollValue<7);
 		
+		int rollValue=diceCup.shakeOneDie(player1);
+		
+		result=(0<rollValue && rollValue<7);
+
 		assertEquals(expected,result);
 	}
-	
+
 	@Test
 	public final void testDiceCupShakeTwoDice(){
 		boolean expected = true;
 		boolean result;
-		int rollValue=diceCup.shakeTwoDice(player1);
-		result=(1<rollValue && rollValue<13);
 		
+		int rollValue=diceCup.shakeTwoDice(player1);
+		
+		result=(1<rollValue && rollValue<13);
+
 		assertEquals(expected,result);
 	}
-	
+
 	@Test
 	public final void testTC11TC14(){
 		boolean expected = true;
 		boolean result;
-		for (int n=0;n<=12000;++n){
-		int rollValue=diceCup.onlyShakeTwoDice(player1);
-		result=(1<rollValue && rollValue<13);
-		assertEquals(expected,result);
-		}
 		
+		for (int n=0;n<=12000;++n){
+			
+			int rollValue=diceCup.onlyShakeTwoDice(player1);
+			
+			result=(1<rollValue && rollValue<13);
+			
+			assertEquals(expected,result);
+		}
+
 	}
 
 }
